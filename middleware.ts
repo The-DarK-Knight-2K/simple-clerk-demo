d4ffad8d60cf12806b0a33eedaaacc33af1abb36
The-DarK-Knight-2K <ranujajayawardena@gmail.com>
2025-12-04 19:46:54 +0530

    database now works with some errors

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api/webhooks/clerk', // Make webhook route public
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
